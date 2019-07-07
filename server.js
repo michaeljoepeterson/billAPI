a638f18fe9e2b7b9b0699849a8cee323d1252642
Michael Peterson <michaeljoepeterson@gmail.com>
2019-07-07 16:18:15 -0600

    fixeed cors

--- server.js
+++ server.js
@@ -3,24 +3,25 @@ const express = require('express');
 const {PORT, DATABASE_URL } = require('./config');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const jsonParser = bodyParser.json();
 const {router:getBillRouter} = require('./routers/billRouter');
 const {router:getBillDataRouter} = require('./routers/getBills');
 const app = express();
 
-app.use(jsonParser);
-app.use("/getBills",getBillRouter);
-app.use("/api/bills",getBillDataRouter);
-
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   if (req.method === 'OPTIONS') {
     return res.send(204);
   }
   next();
 });
+
+app.use(jsonParser);
+app.use("/getBills",getBillRouter);
+app.use("/api/bills",getBillDataRouter);
 
 function runServer( databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
