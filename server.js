600c154dc228644cfc4147ca992f531defa730f8
Michael Peterson <michaeljoepeterson@gmail.com>
2019-07-07 16:09:11 -0600

    added cors

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(jsonParser);
 app.use("/getBills",getBillRouter);
 app.use("/api/bills",getBillDataRouter);
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
 function runServer( databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl,{ useNewUrlParser: true },err => {
